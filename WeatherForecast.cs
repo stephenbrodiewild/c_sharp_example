1bdfd8182e6ad4c9b24b0f0dbbcc45e857ed0e76
sbw <stephenbrodiewild@gmail.com>
2023-10-18 13:46:10 +0800

    initial commit; built from dotnet template

---
+++ WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace c_sharp_test;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
